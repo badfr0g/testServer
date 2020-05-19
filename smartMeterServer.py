9486fcf1e5fc06500eae1ffaec3e76a0e67c0631
Joseph <ariesyang92@gmail.com>
2020-05-19 15:26:50 +0800

    Add files via upload

---
+++ smartMeterServer.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import serial
+import time
+import threading
+app = Flask(__name__)
+
+ser = serial.Serial('/dev/ttyS1', 9600)
+
+def get_data():
+    while True:
+        global x 
+        x = ser.readline()
+
+thread = threading.Thread(target = get_data)
+thread.start()
+
+@app.route('/')
+def homePage():
+    return x
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 80)
